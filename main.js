15ded1b52f5b5e30bae34c3b6a63db447c816146
IvanaTijanic <45176652+IvanaTijanic@users.noreply.github.com>
2020-01-25 17:08:36 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -3,33 +3,35 @@ const root = document.getElementById("root")
 fetch('https://swapi.co/api/species/')
     .then(response => response.json())
     .then(x => {
-    let sablon =""
-    let niz = x.results
-    for(let i=0; i < niz.length; i++){
-        const user = niz[i]
-       console.log(user)
+        let sablon = ""
+        let niz = x.results
+        for (let i = 0; i < niz.length; i++) {
+            const user = niz[i]
+            console.log(user)
 
-       sablon += `
+            sablon += `
     
        <div class="vrste">
+       <div class="holePunch">
+           <span class="dot"></span>
            <h1 style="text-align:center;">${user.name}</h1>
+        </div>
 
-           <img src="http://starwars-visualguide.com/assets/img/species/${i+5}.jpg">
+           <img src="http://starwars-visualguide.com/assets/img/species/${i + 5}.jpg">
 
-           <p> Klasifikacija: ${user.classification}</p>
-           <p> Ishrana: ${user.designation}</p>
-           <p> Prosecna visina: ${user.average_height}</p>
-           <p> Boja kože: ${user.skin_colors}</p>
-           <p> Boja kose: ${user.hair_colors}</p>
-           <p> Boja ociju: ${user.eye_colors}</p>
-           <p> Prosecna starost: ${user.average_lifespan}</p>
-           <p> Govorni jezik: ${user.language}</p>
-       </div>`  
-       console.log(sablon);
-     
-       
-     } 
-     root.innerHTML += sablon
-})
+           <p class="details"> Classification:<span class="detailsResults"> ${user.classification}</span></p>
+           <p class="details"> Designation:<span class="detailsResults"> ${user.designation}</span></p>
+           <p class="details"> Average height:<span class="detailsResults"> ${user.average_height}</span></p>
+           <p class="details"> Skin colors:<span class="detailsResults"> ${user.skin_colors}</span></p>
+           <p class="details"> Hair colors:<span class="detailsResults"> ${user.hair_colors}</span></p>
+           <p class="details"> Eye colors:<span class="detailsResults"> ${user.eye_colors}</span></p>
+           <p class="details"> Average lifespan:<span class="detailsResults"> ${user.average_lifespan}</span></p>
+           <p class="details"> Language:<span class="detailsResults"> ${user.language}</span></p>
+       </div>`
+            console.log(sablon);
 
-  
+
+        }
+        root.innerHTML += sablon
+    })
+
