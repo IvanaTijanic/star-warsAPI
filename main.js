496e154f9ae8eb581219c21a1c11d67ec10cf235
Ivana Tijanic <ivana.tijanic@saasuma.com>
2021-07-22 17:36:50 +0200

    changed doamin for images

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ fetch('https://swapi.dev/api/people/')
            <h1 style="text-align:center;">${user.name}</h1>
         </div>
 
-           <img src="http://starwars-visualguide.com/assets/img/people/${i + 5}.jpg">
+           <img src="http://starwars-visualguide.com/assets/img/species/${i + 5}.jpg">
 
            <p class="details"> Classification:<span class="detailsResults"> ${user.classification}</span></p>
            <p class="details"> Designation:<span class="detailsResults"> ${user.designation}</span></p>
